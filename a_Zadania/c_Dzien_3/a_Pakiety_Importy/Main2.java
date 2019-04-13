28d84446361bf55bd9c32d8ff5079170ca9a1776
Igor Kołodziejczyk <igor.k.kolodziejczyk@gmail.com>
2019-04-13 09:06:55 +0200

    Rozwiązanie zadań z dzień 3 sekcja a

--- a_Zadania/c_Dzien_3/a_Pakiety_Importy/Main2.java
+++ a_Zadania/c_Dzien_3/a_Pakiety_Importy/Main2.java
@@ -6,6 +6,11 @@ public class Main2 {
 
     public static void main(String[] args) {
 
+        java.util.Date date = new java.util.Date();
+        System.out.println(date);
+
+        //Extra: SQL date stores years, months and days while hour, minute, second and millisecond are ignored
+        System.out.println(new Date(1234));
 
     }
 
